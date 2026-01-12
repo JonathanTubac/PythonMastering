7ae0e84b450c64a84e800d8dcd0e37fd7f12efb6
JonathanTubac <javiertubac1290.e@gmail.com>
2026-01-12 10:38:36 -0600

    Section 36 added

---
+++ 36-Flask/hello.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "HEllooo"
+@app.route("/bye")
+def bye():
+    return "Bye!"
+if __name__ == "__main__":
+    app.run()
